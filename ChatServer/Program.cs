72017b2b636ccbd10e0dea81e5063552ff23901c
Alexander Woods <32915873+AlxWoods@users.noreply.github.com>
2023-03-26 23:38:13 -0500

    now sends packet to server, reads, and then uses it

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -9,14 +9,23 @@ namespace ChatServer
 
     class Program 
     {
+        static List<Client> _users;
         static TcpListener _listener;
         static void Main(string[] args) 
         {
+            _users = new List<Client>();
             _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 30005);
             _listener.Start();
-        
-            var client = _listener.AcceptTcpClient();
-            Console.WriteLine("Client has Connected");
+
+            while (true)
+            {
+                var client = new Client(_listener.AcceptTcpClient());
+                _users.Add(client);
+
+                /* Boradcast the connection*/
+            }
+
+
         }
     }
 
